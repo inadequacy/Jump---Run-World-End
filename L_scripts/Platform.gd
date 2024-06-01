c666ade94bbf147b0ea98a358667f5f918451cb8
Levent <cracea.levent@gmail.com>
2024-06-01 19:10:27 +0200

    hope it doesn't break

--- L_scripts/Platform.gd
+++ L_scripts/Platform.gd
@@ -1,11 +1,17 @@
-extends Area2D
-
-
+extends StaticBody2D
+@onready var _animated_sprite = $PLAAAAAT
+@onready var _platform = $CollisionShape2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_animated_sprite.stop()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_area_2d_body_entered(body):
+	_animated_sprite.play("default")
+	await get_tree().create_timer(1.0).timeout
+	_platform.disabled = true
+	 # Replace with function body.
