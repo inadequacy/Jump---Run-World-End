0be39e16a9d1b6fa19e183d903622fb9b00a8a45
inadequacy <cole.brookslyon@gmail.com>
2024-06-01 19:59:36 +0200

    level lost sound attached

--- MainScene.gd
+++ MainScene.gd
@@ -2,6 +2,7 @@ extends Node2D
 @onready var player = $PlayerCharacter
 @onready var start = $Start.global_position
 @onready var levelComplete = $WinCondition/restart
+@onready var levelLost = $LoseCondition/death
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -18,5 +19,12 @@ func _on_win_condition_area_entered(area):
 
 func _on_win_condition_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	levelComplete.play()
+	await get_tree().create_timer(2.0).timeout
+	player.global_position = start
+
+
+
+func _on_lose_condition_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	levelLost.play()
 	await get_tree().create_timer(2.0).timeout
 	player.global_position = start
